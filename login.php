a924dec90fe721f85c2f9aff3aba017dd9570061
indahpaendong <indahpaendong026@student.unsrat.ac.id>
2025-05-26 22:01:59 +0800

    Update login.php

--- login.php
+++ login.php
@@ -28,7 +28,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <html>
 <head>
     <title>Login - Booking Studio</title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="stylelogin.css">
 </head>
 <body>
     <h2>Login</h2>
