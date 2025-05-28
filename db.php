9ca332b261b7caae9d6bcf17c0d4594857928ee0
indahpaendong <indahpaendong026@student.unsrat.ac.id>
2025-05-28 12:57:19 +0800

    Update db.php

--- db.php
+++ db.php
@@ -1,10 +1,15 @@
 <?php
+// Aktifkan laporan error MySQLi saat pengembangan
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
 $host = "localhost";
 $user = "root";
 $pass = "";
 $db = "booking_studio";
 
 $conn = new mysqli($host, $user, $pass, $db);
+$conn->set_charset("utf8mb4"); // set karakter encoding
+
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
 }
